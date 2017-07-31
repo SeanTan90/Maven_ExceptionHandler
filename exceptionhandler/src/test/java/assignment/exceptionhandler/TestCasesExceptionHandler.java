a53a61eeaf60801e85b5f6099ce62715d038e3ef
Sean Tan Jui Siang <sean.a.tan@capgemini.com>
2017-07-31 14:46:47 +0800

    Added sample test case

---
+++ exceptionhandler/src/test/java/assignment/exceptionhandler/TestCasesExceptionHandler.java
@@ -0,0 +1,14 @@
+package assignment.exceptionhandler;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestCasesExceptionHandler {
+
+	@Test
+	public void test() {
+
+	}
+
+}
